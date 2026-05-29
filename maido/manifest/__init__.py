9e91028960b987a5c0a4b54e75633c9b5b74c3d8
Mark Wingerd <mawingerd@gmail.com>
2026-05-29 15:17:53 -0700

    Update init

--- maido/manifest/__init__.py
+++ maido/manifest/__init__.py
@@ -3,9 +3,12 @@ from .schema import (
     validate_manifest,
     validate_manifest_against_probe,
 )
+from .template import build_manifest_data, write_manifest_file
 
 __all__ = [
+    "build_manifest_data",
     "load_manifest_file",
     "validate_manifest",
     "validate_manifest_against_probe",
+    "write_manifest_file",
 ]
