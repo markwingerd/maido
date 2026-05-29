a87eff45ee2f60eee393e7e211c2e97625ffa912
Mark Wingerd <mawingerd@gmail.com>
2026-05-29 16:45:07 -0700

    Add crop

--- maido/__init__.py
+++ maido/__init__.py
@@ -1,10 +1,13 @@
-from maido.bundle.inspection import inspect_bundle_path
-from maido.bundle.packing import pack_bundle
-from maido.manifest.template import build_manifest_data, write_manifest_file
+from .bundle.inspection import inspect_bundle_path
+from .bundle.packing import pack_bundle
+from .crop import plan_crop, plan_crop_for_manifest
+from .manifest.template import build_manifest_data, write_manifest_file
 
 __all__ = [
     "build_manifest_data",
     "inspect_bundle_path",
     "pack_bundle",
+    "plan_crop",
+    "plan_crop_for_manifest",
     "write_manifest_file",
 ]
