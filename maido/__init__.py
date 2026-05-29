ad9111c3ba9adf962d7252fa79f526b0524d1c41
Mark Wingerd <mawingerd@gmail.com>
2026-05-29 15:18:07 -0700

    Update init

--- maido/__init__.py
+++ maido/__init__.py
@@ -1,3 +1,10 @@
 from maido.bundle.inspection import inspect_bundle_path
+from maido.bundle.packing import pack_bundle
+from maido.manifest.template import build_manifest_data, write_manifest_file
 
-__all__ = ["inspect_bundle_path"]
+__all__ = [
+    "build_manifest_data",
+    "inspect_bundle_path",
+    "pack_bundle",
+    "write_manifest_file",
+]
